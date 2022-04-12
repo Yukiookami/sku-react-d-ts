05b5ff35c144f3203d4206f481644a1ab2494a62
Yukiookami <37170227+Yukiookami@users.noreply.github.com>
2022-04-12 19:28:34 +0900

    完成初版button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,31 @@
 /*
  * @Author: zxy
  * @Date: 2022-04-11 14:33:08
- * @LastEditTime: 2022-04-11 15:27:19
+ * @LastEditTime: 2022-04-12 17:01:09
  * @FilePath: /sku-react-d/src/App.tsx
  */
 import React from 'react';
+import SkuButton, { ButtonType, ButtonSize } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
-        <h2>Hello world</h2>
-        <h3>Hello world</h3>
-        <h4>Hello world</h4>
-        <h5>Hello world</h5>
+        <SkuButton>hello</SkuButton>
 
-        <code>
-          const a = b
-        </code>
+        <SkuButton disabled>Disabled Button</SkuButton>
 
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SkuButton size={ButtonSize.Large}>Large Button</SkuButton>
+
+        <SkuButton size={ButtonSize.Small}>Small Button</SkuButton>
+
+        <SkuButton btnType={ButtonType.Primary}>Primary Button</SkuButton>
+
+        <SkuButton btnType={ButtonType.Danger}>Danger Button</SkuButton>
+
+        <SkuButton btnType={ButtonType.Link} href="www.bilibili.com">Link Button</SkuButton>
+
+        <SkuButton btnType={ButtonType.Link} href="www.bilibili.com" disabled>Link Button disabled</SkuButton>
       </header>
     </div>
   );
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,78 @@
+/*
+ * @Author: zxy
+ * @Date: 2022-04-12 13:48:03
+ * @LastEditTime: 2022-04-12 19:20:36
+ * @FilePath: /sku-react-d/src/components/Button/button.tsx
+ */
+import React from "react";
+import classNames from 'classnames'
+
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
+}
+
+interface BaseBttonProps {
+  className?: string,
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?:string
+}
+
+// 让组件拥有原生属性
+type NativeButtonProps = React.ButtonHTMLAttributes<HTMLElement> & BaseBttonProps
+type AnchorButtonProps = React.AnchorHTMLAttributes<HTMLElement> & BaseBttonProps
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const SkuButton: React.FC<ButtonProps> = (props) => {
+  const { 
+    btnType,
+    className,
+    disabled,
+    size,
+    children,
+    href,
+    ...restProps
+  } = props
+
+  // 默认btn
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === ButtonType.Link) && disabled
+  })
+
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a 
+        className={classes}
+        href={href}
+        {...restProps}
+      >{ children }</a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+        {...restProps}
+      >{ children }</button>
+    )
+  }
+}
+
+SkuButton.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+
+export default SkuButton
